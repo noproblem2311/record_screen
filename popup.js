8505b8a8b398ce9412e846f622c0ca5db51e8080
noproblem2311 <quanpva.dev@gmail.com>
2024-02-02 13:35:15 +0700

    setup update

--- popup.js
+++ popup.js
@@ -1,89 +1,5 @@
 
 
-// document.addEventListener("DOMContentLoaded", function () {
-//     let mediaRecorder;
-//     let videoData = new FormData(); // Initialize FormData to accumulate video chunks
-
-//     const startButton = document.getElementById("startRecording");
-//     const stopButton = document.getElementById("stopRecording");
-//     const infoBox = document.getElementById("infoBox");
-
-//     function handleMediaAccessDenied() {
-//         infoBox.textContent = "Access to display media was denied. Please allow access to use the recording feature.";
-//         startButton.disabled = false;
-//         stopButton.disabled = true;
-//     }
-
-//     startButton.addEventListener("click", () => {
-//         navigator.mediaDevices.getDisplayMedia({ audio: true, video: true })
-//             .then((stream) => {
-//                 mediaRecorder = new MediaRecorder(stream);
-//                 mediaRecorder.ondataavailable = (event) => {
-//                     if (event.data.size > 0) {
-//                         console.log("check");
-//                         videoData.append("chunks", event.data);
-//                         // if ( videoData.size > 500){
-//                         //     console.log("start sending sub video");
-//                         //     fetch('http://localhost:8000/handle_video/', { 
-//                         //         method: 'POST',
-//                         //         body: {
-//                         //             chunks: videoData,
-//                         //             isrecording: true
-//                         //         }
-//                         //     })
-//                         //     .then(response => response.json())
-//                         //     .then(data => {
-//                         //         console.log('Video uploaded:', data);
-//                         //     })
-//                         //     .catch(error => {
-//                         //         console.error('Error uploading video:', error);
-//                         //     });
-
-//                         //     videoData = new FormData();
-//                         // }
-//                     }
-//                 };
-
-//                 mediaRecorder.onstop = () => {
-                    
-//                     fetch('http://localhost:8000/handle_video/', { 
-//                         method: 'POST',
-//                         body: videoData
-//                     })
-//                     .then(response => response.json())
-//                     .then(data => {
-//                         console.log('Video uploaded:', data);
-//                     })
-//                     .catch(error => {
-//                         console.error('Error uploading video:', error);
-//                     });
-
-//                     videoData = new FormData();
-//                 };
-
-//                 mediaRecorder.start();
-//                 startButton.disabled = true;
-//                 stopButton.disabled = false;
-//                 infoBox.textContent = "";
-//             })
-//             .catch((error) => {
-//                 console.error("Error accessing display media:", error);
-//                 handleMediaAccessDenied();
-//             });
-//     });
-
-//     stopButton.addEventListener("click", () => {
-//         if (mediaRecorder && mediaRecorder.state === "recording") {
-//             mediaRecorder.stop();
-//             startButton.disabled = false;
-//             stopButton.disabled = true;
-//         }
-//     });
-// });
-
-
-
-
 
 
 
@@ -96,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const startButton = document.getElementById("startRecording");
     const stopButton = document.getElementById("stopRecording");
     const infoBox = document.getElementById("infoBox");
-    const MAX_CHUNK_SIZE = 1 * 1024 * 1024; // 1MB
+    const MAX_CHUNK_SIZE = 1024*100; // 1MB
 
     function sendVideoData() {
         // Gửi videoData đến server
@@ -127,6 +43,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
                         // Kiểm tra nếu chunkSize lớn hơn hoặc bằng MAX_CHUNK_SIZE thì gửi dữ liệu
                         if (chunkSize >= MAX_CHUNK_SIZE) {
+                            console.log("Sending sub video data...");
                             sendVideoData();
                         }
                     }
